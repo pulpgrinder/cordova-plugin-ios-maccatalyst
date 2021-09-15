6c4e99e80ef16bc646e575eb02353003a1298486
Pulpgrinder <pulpgrinder@protonmail.com>
2021-09-15 09:51:27 -0800

    Remove Obj-C files.

--- src/ios/CDVMacCatalyst.h
+++
@@ -1,9 +0,0 @@
-/********* CDVCatalyst.h Cordova Plugin Header *******/
-
-#import <Cordova/CDVPlugin.h>
-
-@interface CDVMacCatalyst : CDVPlugin
-
-- (void)isMacCatalyst:(CDVInvokedUrlCommand*)command;
-
-@end
