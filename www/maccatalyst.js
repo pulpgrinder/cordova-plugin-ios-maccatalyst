2551c460ccbaf2c846c36bb09d877d2c838f53cd
Pulpgrinder <pulpgrinder@protonmail.com>
2021-09-15 09:52:37 -0800

    Expanded API.

--- www/maccatalyst.js
+++ www/maccatalyst.js
@@ -1,7 +1,18 @@
-cordova.maccatalyst = {
-  isMacCatalyst:function(callback) {
-      cordova.exec(callback, function(err) {
-          callback('error in isMacCatalyst');
-      }, "MacCatalyst", "isMacCatalyst", []);
-  }
+var exec = require('cordova/exec');
+exports.isMacCatalyst = function(success) {
+  exec(success, function(err){console.log(err)}, 'maccatalyst', 'isMacCatalyst', []);
 };
+exports.pickDocument = function(types,success) {
+  exec(success, function(err){console.log(err)}, 'maccatalyst', 'pickDocument', [types,success]);
+}
+exports.getMacCatalystContainerDocumentsFolder = function(success) {
+  exec(success, function(err){console.log(err)}, 'maccatalyst', 'getMacCatalystContainerDocumentsFolder', []);
+};
+exports.intializeMacCatalystPlugin = function(){
+  maccatalyst.isMacCatalyst(function(result){
+    maccatalyst.hasMacCatalyst = result;
+  })
+  maccatalyst.getMacCatalystContainerDocumentsFolder(function(folderURL){
+    maccatalyst.containerDocumentsFolder = folderURL;
+  })
+}
